0397a47966462f3e1ae0a6f911f52ddc8f30ca43
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-01-08 17:43:02 +0400

    done

--- dtpkr/ContentView.swift
+++ dtpkr/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedDate = Date()
+    @State private var showDatePicker = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            DatePicker("", selection: self.$selectedDate, displayedComponents: .date)
+                .labelsHidden()
+                .datePickerStyle(GraphicalDatePickerStyle())                
+            
+            Text("\(selectedDate, formatter: dateFormatter)")
+
+            Button("Copy and close") {
+                print(">>> \(dateFormatter.string(from: selectedDate))")
+                copyToClipboard()
+                NSApplication.shared.terminate(self)
+            }
+            .keyboardShortcut(.defaultAction)
         }
         .padding()
+        .onReceive(NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification), perform: { _ in
+            NSApp.mainWindow?.standardWindowButton(.zoomButton)?.isHidden = true
+            NSApp.mainWindow?.standardWindowButton(.closeButton)?.isHidden = true
+            NSApp.mainWindow?.standardWindowButton(.miniaturizeButton)?.isHidden = true
+        })
     }
+
+    func copyToClipboard() {
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(dateFormatter.string(from: selectedDate), forType: .string)
+    }
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
 }
 
 struct ContentView_Previews: PreviewProvider {
