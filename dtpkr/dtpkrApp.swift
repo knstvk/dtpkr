4c32ae0598d25ffb6e2e5282158b1f3a48f5dab9
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-01-08 14:34:36 +0400

    Initial Commit

---
+++ dtpkr/dtpkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  dtpkrApp.swift
+//  dtpkr
+//
+//  Created by Konstantin on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct dtpkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
